abbddbe2cb4156b45146ff5737fc3c31172c1f61
Jordan Lord <jordanpaullord@hotmail.co.uk>
2015-09-15 19:37:42 +0100

    Add array2D function to create a 2d array for the grid

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
+function array2D(width, height) {
+	var returnArray = [];
+
+	for (var x = 0; x < width; x++) {
+		returnArray[x] = [];
+
+		for (var y = 0; y < height; y++) {
+			returnArray[x][y] = false;
+		}
+	}
+
+	return returnArray;
+}
+
+
 (function () {
 	var canvas = document.getElementById("cnvsMain");
 	var ctx = canvas.getContext("2d");
+
+	var universe = array2D(10, 10);
+
+	console.log(universe[5][5] || "Hello");
 }());
