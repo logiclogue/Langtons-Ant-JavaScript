777ff877c197fe846196c767c526aab699530e64
Jordan Lord <jordanpaullord@hotmail.co.uk>
2015-09-17 21:42:04 +0100

    Add speed variable

--- js/main.js
+++ js/main.js
@@ -5,16 +5,17 @@
 	var universe = new Array2D();
 	var ant = new LangtonAnt();
 	var paint = new LangtonCanvas();
+	var speed = 1000;
 
 	paint.setCanvas("cnvsMain");
 
 
 	setInterval(function () {
-		for (var i = 0; i < 1; i += 1) {
+		for (var i = 0, max = speed / 1000; i < max; i += 1) {
 			paint.draw(ant.px, ant.py, (universe.get(ant.px, ant.py) ? "ON" : "OFF"));
 			paint.draw(ant.x, ant.y, "ANT");
 
 			universe.set(ant.x, ant.y, ant.update(universe.get(ant.x, ant.y)));
 		}
-	}, 1);
+	}, 1000 / speed);
 }());
