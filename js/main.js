9922453e51478e44eadd58894ac8c93f1083e471
Jordan Lord <jordanpaullord@hotmail.co.uk>
2015-09-17 22:01:04 +0100

    Add multiple ants

--- js/main.js
+++ js/main.js
@@ -2,20 +2,32 @@
 
 
 (function () {
+	var ants = [];
 	var universe = new Array2D();
-	var ant = new LangtonAnt();
 	var paint = new LangtonCanvas();
 	var speed = 1000;
 
+	for (var i = 0; i < 10; i += 1) {
+		var ant = new LangtonAnt();
+
+		ant.x = Math.round((Math.random() - 5) * 10);
+		ant.y = Math.round((Math.random() - 5) * 10);
+
+		ants.push(ant);
+	}
+
 	paint.setCanvas("cnvsMain");
+	paint.scale = 1;
 
 
 	setInterval(function () {
 		for (var i = 0, max = speed / 1000; i < max; i += 1) {
-			paint.draw(ant.px, ant.py, (universe.get(ant.px, ant.py) ? "ON" : "OFF"));
-			paint.draw(ant.x, ant.y, "ANT");
+			for (var j = 0, max = ants.length; j < max; j += 1) {
+				paint.draw(ants[j].px, ants[j].py, (universe.get(ants[j].px, ants[j].py) ? "ON" : "OFF"));
+				paint.draw(ants[j].x, ants[j].y, "ANT");
 
-			universe.set(ant.x, ant.y, ant.update(universe.get(ant.x, ant.y)));
+				universe.set(ants[j].x, ants[j].y, ants[j].update(universe.get(ants[j].x, ants[j].y)));
+			}
 		}
 	}, 1000 / speed);
 }());
