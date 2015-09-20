ceb2cdfc3eb31b946e0d96909645886b6c023bc4
Jordan Lord <jordanpaullord@hotmail.co.uk>
2015-09-20 13:32:41 +0100

    Add zoom and move around using -, + and arrow keys Add poor screen redraw

--- js/langton-canvas.js
+++ js/langton-canvas.js
@@ -29,6 +29,42 @@ var LangtonCanvas = function () {
 
 	self.draw = function (x, y, thingToDraw) {
 		ctx.fillStyle = self.colour[thingToDraw];
-		ctx.fillRect(Math.round(x * self.scale + (canvas.width / 2) - (self.scale / 2)), Math.round(y * self.scale + (canvas.height / 2) - (self.scale / 2)), Math.round(self.scale), Math.round(self.scale));
+		ctx.fillRect(Math.round((x + self.pos_x) * self.scale + (canvas.width / 2) - (self.scale / 2)), Math.round((y + self.pos_y) * self.scale + (canvas.height / 2) - (self.scale / 2)), Math.ceil(self.scale), Math.ceil(self.scale));
 	};
+
+	self.redraw = function () {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	};
+
+	document.addEventListener("keydown", function(e) {
+		var boolKeyPressed = true;
+
+		switch (e.keyCode) {
+			case 189: // zoom out
+				self.scale = self.scale / 2;
+				break;
+			case 187: // zoom in
+				self.scale = self.scale * 2;
+				break;
+			case 37: // left
+				self.pos_x += 100 / self.scale;
+				break;
+			case 39: // right
+				self.pos_x -= 100 / self.scale;
+				break;
+			case 38: // up
+				self.pos_y += 100 / self.scale;
+				break;
+			case 40: // down
+				self.pos_y -= 100 / self.scale;
+				break;
+			default:
+				boolKeyPressed = false;
+				break;
+		}
+
+		if (boolKeyPressed) {
+			self.redraw();
+		}
+	});
 };
