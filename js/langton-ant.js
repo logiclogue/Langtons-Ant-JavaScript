266cd7488a163af4db7c703712cd57de3d63dab5
logiclogue <jordanpaullord@hotmail.co.uk>
2015-10-22 07:45:18 +0100

    Update langton-ant class

--- js/langton-ant.js
+++ js/langton-ant.js
@@ -1,50 +1,52 @@
-var LangtonAnt = function () {
-	var self = this;
-
-
-	self.x = 0;
-	self.y = 0;
-	self.px = 0;
-	self.py = 0;
-	self.direction = 0;
+var LangtonAnt = (function () {
+	function LangtonAnt() {
+		this.x = 0;
+		this.y = 0;
+		this.px = 0;
+		this.py = 0;
+		this.direction = 0;
+	}
 	
 
 	// update its position and return updated value for the current square
-	self.update = function (currentSquare) {
-		var preDirection = self.direction;
+	LangtonAnt.prototype.update = function (currentSquare) {
+		var preDirection = this.direction;
 
-		self.px = self.x;
-		self.py = self.y;
+		this.px = this.x;
+		this.py = this.y;
 
 		// update direction
 		// if current square is 'on'
 		if (currentSquare) {
 			// then turn ant left
-			self.direction = (preDirection < 1 ? 3 : preDirection - 1);
+			this.direction = (preDirection < 1 ? 3 : preDirection - 1);
 		}
 		// if current square is 'off'
 		else {
 			// then turn ant right
-			self.direction = (preDirection > 2 ? 0 : preDirection + 1);
+			this.direction = (preDirection > 2 ? 0 : preDirection + 1);
 		}
 
 		// update position
-		switch (self.direction) {
+		switch (this.direction) {
 			case 0: // up
-				self.y -= 1;
+				this.y -= 1;
 				break;
 			case 1: // right
-				self.x += 1;
+				this.x += 1;
 				break;
 			case 2: // down
-				self.y += 1;
+				this.y += 1;
 				break;
 			case 3: // left
-				self.x -= 1;
+				this.x -= 1;
 				break;
 		}
 
 		// return the value of the updated square
 		return !currentSquare;
 	};
-};
+
+
+	return LangtonAnt;
+}());
